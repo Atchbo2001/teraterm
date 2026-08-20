416bf9bc245d5115e87fd9c183ceb4d95682b9c7
Atchbo <86882671+Atchbo2001@users.noreply.github.com>
2026-08-20 13:49:57 -0500

    test: cover one-shot disconnect origin consumption

--- teraterm/teraterm/session_state_test.cpp
+++ teraterm/teraterm/session_state_test.cpp
@@ -26,10 +26,28 @@ static void test_authentication_failure_stays_open_and_offers_recovery()
     assert(!transition.close_window);
 }
 
+static void test_pending_user_disconnect_is_consumed_once()
+{
+    DisconnectOrigin pending = DisconnectOrigin::UserRequested;
+    const DisconnectOrigin origin = ConsumeDisconnectOrigin(&pending, DisconnectOrigin::RemoteOrNetwork);
+    assert(origin == DisconnectOrigin::UserRequested);
+    assert(pending == DisconnectOrigin::None);
+}
+
+static void test_missing_pending_origin_uses_fallback()
+{
+    DisconnectOrigin pending = DisconnectOrigin::None;
+    const DisconnectOrigin origin = ConsumeDisconnectOrigin(&pending, DisconnectOrigin::RemoteOrNetwork);
+    assert(origin == DisconnectOrigin::RemoteOrNetwork);
+    assert(pending == DisconnectOrigin::None);
+}
+
 int main()
 {
     test_remote_disconnect_stays_open_and_offers_recovery();
     test_user_disconnect_stays_open_without_recovery_nag();
     test_authentication_failure_stays_open_and_offers_recovery();
+    test_pending_user_disconnect_is_consumed_once();
+    test_missing_pending_origin_uses_fallback();
     return 0;
 }
