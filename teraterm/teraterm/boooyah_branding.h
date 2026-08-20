5409e30d074bdd48cfc1f6d8b923c58c812d9211
Atchbo <86882671+Atchbo2001@users.noreply.github.com>
2026-08-20 13:54:38 -0500

    feat: centralize Boooyah product branding

---
+++ teraterm/teraterm/boooyah_branding.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define BOOOYAH_PRODUCT_NAME_A "Boooyah"
+#define BOOOYAH_PRODUCT_NAME_W L"Boooyah"
+#define BOOOYAH_WINDOW_PREFIX_A "Boooyah"
+#define BOOOYAH_WINDOW_PREFIX_W L"Boooyah"
+#define BOOOYAH_UPSTREAM_ATTRIBUTION_W L"Based on Tera Term and TTSSH by the TeraTerm Project"
