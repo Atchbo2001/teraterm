1ab57ee179aae58d374b1b8a39dae137dbd117f3
Atchbo <86882671+Atchbo2001@users.noreply.github.com>
2026-08-20 11:27:24 -0500

    refactor: make session policy header-only

--- teraterm/teraterm/session_state.h
+++ teraterm/teraterm/session_state.h
@@ -22,4 +22,29 @@ struct SessionTransition {
     bool close_window;
 };
 
-SessionTransition HandleDisconnect(SessionState current, DisconnectOrigin origin);
+inline SessionTransition HandleDisconnect(SessionState current, DisconnectOrigin origin)
+{
+    SessionTransition transition{};
+    transition.close_window = false;
+
+    switch (origin) {
+    case DisconnectOrigin::UserRequested:
+        transition.next = SessionState::Disconnected;
+        transition.show_recovery = false;
+        break;
+
+    case DisconnectOrigin::Authentication:
+        transition.next = current == SessionState::Connecting ? SessionState::Failed : SessionState::Disconnected;
+        transition.show_recovery = true;
+        break;
+
+    case DisconnectOrigin::RemoteOrNetwork:
+    case DisconnectOrigin::None:
+    default:
+        transition.next = SessionState::Disconnected;
+        transition.show_recovery = true;
+        break;
+    }
+
+    return transition;
+}
